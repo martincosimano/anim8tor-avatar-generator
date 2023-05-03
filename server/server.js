8718de290c55320e5039a3f84605ca869ff939f8
martincosimano <martincosimano@hotmail.com>
2023-05-03 20:07:48 -0300

    server connected to mongodb

--- server/server.js
+++ server/server.js
@@ -1,18 +1,13 @@
 const express = require('express');
-const cors = require('cors');
+const dotenv = require('dotenv')
+const connectDB = require('./config/db')
+
+dotenv.config( { path: './config/config.env'})
+
+connectDB()
 
 const app = express();
 
-app.get('/api/customers', cors(), (req, res) => {
-  const customers = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
-  ];
+const PORT = process.env.PORT || 5000;
 
-  res.json(customers);
-});
-
-const port = 5000;
-
-app.listen(port, () => `Server running on port ${port}`);
+app.listen(PORT, () => `Server running on port ${PORT}`);
