3b8aee5cc49e41380f115c2512543db7054fa658
Martín Cosimano <martincosimano@hotmail.com>
2023-06-06 17:07:21 -0300

    Update Footer.js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,7 +4,7 @@ import './footer.css'
 export default function Footer() {
     return (
         <footer>
-            <small className="copyright">© <a href="https://martincosimano.github.io/portfolio/">Martin Cosimano</a>. All rights reserved.</small>
+            <small className="copyright">© <a href="https://martincosimano.github.io/portfolio/">Martin Cosimano</a>.</small>
         </footer>
     )
 }
